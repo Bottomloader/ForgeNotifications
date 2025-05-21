575746bc9add277e79cd1252011104f901154db6
Bottomloader <randomacc.for1randomthings@gmail.com>
2025-05-21 08:58:29 +0000

    meow

--- src/classes/managers/CommandManager.ts
+++ src/classes/managers/CommandManager.ts
@@ -1,13 +1,8 @@
 import { BaseCommandManager } from '@tryforge/forgescript'
-//import type { Events } from 'youtubei'
 
 
 export const handlerName = "ForgeNotifications"
 
-// commented out this verion to get tests running, no `Events` in 'youtubei'
-// export class YoutubeManager extends BaseCommandManager<keyof Events> {
-//      handlerName = 'YTCommands'
-// }
 export class YoutubeManager extends BaseCommandManager<any> {
      handlerName = 'YTCommands'
 }
--- src/classes/structures/ForgeNotifications.ts
+++ src/classes/structures/ForgeNotifications.ts
@@ -1,13 +1,10 @@
 import { EventManager, ForgeClient, ForgeExtension, FunctionManager } from '@tryforge/forgescript'
-//import { ForgeDB } from '@tryforge/forge.db'
 import { YoutubeManager as CommandManager } from '../managers/CommandManager'
-//import Youtube from 'youtubei'
 import { join } from 'path'
 
-export interface YtConfig {
-  // sample property to demonstrate passing values around
-  hello: string
-}
+/* export interface YtConfig {
+  iLoveCock: string
+} */
 
 export class ForgeNotifications extends ForgeExtension {
     name = 'forge.notifications'
@@ -15,11 +12,10 @@ export class ForgeNotifications extends ForgeExtension {
     version = '1.0.0'
     commands!: any
 
-    constructor(public options: YtConfig) {
+    /* constructor(public options: YtConfig) {
       super()
-      // log the option's hello property to the console
-      console.log(options.hello)
-    }
+      console.log(options.iLoveCock)
+    } */
 
     public init(client: ForgeClient): void {
       this.commands = new CommandManager(client);
@@ -27,6 +23,3 @@ export class ForgeNotifications extends ForgeExtension {
       this.load(join(__dirname, '../../natives'));
     }
 }
-
-// this would log 'hello world!' to the console
-let notif = new ForgeNotifications({hello: 'hello world!'})
